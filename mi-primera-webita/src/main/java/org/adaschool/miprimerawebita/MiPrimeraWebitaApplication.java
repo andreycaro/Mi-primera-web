69d649bb37a0139becb72fb087296f0ea0584d67
andreycaro <145929773+andreycaro@users.noreply.github.com>
2023-12-13 22:45:54 -0500

    Mi primera web

---
+++ mi-primera-webita/src/main/java/org/adaschool/miprimerawebita/MiPrimeraWebitaApplication.java
@@ -0,0 +1,13 @@
+package org.adaschool.miprimerawebita;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiPrimeraWebitaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MiPrimeraWebitaApplication.class, args);
+	}
+
+}
---
+++ mi-primera-webita/src/test/java/org/adaschool/miprimerawebita/MiPrimeraWebitaApplicationTests.java
@@ -0,0 +1,13 @@
+package org.adaschool.miprimerawebita;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiPrimeraWebitaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
